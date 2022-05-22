b75561c1b900669edecc5506f63c65fcce606914
urkaaaaa <ura_novikov@mail.ru>
2022-05-22 20:47:43 +0800

    final

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,20 +34,22 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+    let data = { client: { name, phone }, order: { address, sum }, goods: [] };
 
     let countOfGoods = goods.length;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    data.order.address = "ул. " + address.street + ", дом " + address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж, кв " + address.flat;
+    data.order.sum = sum;
+
+    data.client = name + ' ' + phone;
+
+    for (let i = 0; i < countOfGoods; i++) {
+
+        data.goods.push({ title: goods[i].title, count: goods[i].count });
+
     }
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
+    let jsonData = JSON.stringify({ data: data });
 
     return jsonData;
 }
